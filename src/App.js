d0cc58fa4c78567b60fe7b877f86d2b8cb8a4ff9
Mikan <72257910+Mikan-DS@users.noreply.github.com>
2024-09-03 23:50:28 +0300

    Фронт подключен

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+import './css/fonts.css'
+import './css/common.css'
+import './css/text.css'
+import './css/buttons.css'
+import './css/containers.css'
 
 export default function App() {
   return (
